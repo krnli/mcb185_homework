e4122a8b54f3ce486e579866c5880c59b4aab212
krnli <krnli@ucdavis.edu>
2025-01-28 15:14:43 -0800

    update

--- 11oligo.py
+++ 11oligo.py
@@ -2,7 +2,9 @@
 
 def Tm(A, C, G, T):
    length = A + C + G + T
-   if length <= 13: return(A + T) * 2 + (G + C) * 2
-   else: return 64.9 + 41 * (G + C - 16.4) / (A + T + G + C)
+   if length <= 13: 
+      return(A + T) * 2 + (G + C) * 2
+   else: 
+      return 64.9 + 41 * (G + C - 16.4) / (A + T + G + C)
 
 print (Tm(1, 3, 6, 7), end=' C')
--- 20demo.py
+++ 20demo.py
@@ -97,8 +97,6 @@ while True:
     print(euler)
 """
 
-
-"""
 def prime(n):
     for i in range(2, n+1, 1):
         if n / i != 1: return print('not prime')
@@ -113,6 +111,5 @@ prime(7) # prime
 prime(8)
 prime(9)
 
-"""
 
 
