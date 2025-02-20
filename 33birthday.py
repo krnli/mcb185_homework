bf9f72e894a5b3b07e9d4ae7ee4e32f77d70422d
krnli <krnli@ucdavis.edu>
2025-02-20 15:10:53 -0800

    update

---
+++ 33birthday.py
@@ -0,0 +1,17 @@
+import random
+import sys
+
+trials = int(sys.argv[1])
+days = int(sys.argv[2])
+people = int(sys.argv[3])
+
+birthdays = []
+for i in range(people):
+    bd = random.randint(0, days)
+    birthdays.append(bd)
+
+same_birthdays = 0
+for thing in birthdays:
+    if thing in birthdays[thing+1:]: same_birthdays += 1
+
+print(same_birthdays/trials)
