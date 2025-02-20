97f0a51d21e77d987013e4c9ad1279cf1970c978
krnli <krnli@ucdavis.edu>
2025-02-20 15:31:09 -0800

    update

--- ex2.py
+++ ex2.py
@@ -8,6 +8,6 @@ while True:
     n += 1
     x = random.random()
     y = random.random()
-    if y >= x**2: above += 1 
+    if y > x**2: above += 1 
     else: below += 1
     if below > 0: print(f'parabola ratios: {above/below}, {above/n}, {below/n}')
